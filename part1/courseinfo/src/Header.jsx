4e38d7a878c882d637dfacf996dff43a96f09a9c
Eggyf <abdelfregel1711@gmail.com>
2025-02-07 19:38:31 -0500

    1.4

--- part1/courseinfo/src/Header.jsx
+++ part1/courseinfo/src/Header.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 
 const Header = (props) => {
+    console.log(props);
     return (
         <>
+            
             <h1>{ props.course }</h1>
         </>
     )
