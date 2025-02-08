69e5320b51787f1b11979b8f24f91182078357c9
Eggyf <abdelfregel1711@gmail.com>
2025-02-07 22:35:37 -0500

    1.9

--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -1,15 +1,24 @@
 
 const Statistics = (props) => {
+    if (props.good != 0 || props.neutral != 0 || props.bad != 0) {
+        return (
+            <>
+                <h2>Statistics</h2>
+                <p>good {props.good}</p>
+                <p>neutral {props.neutral}</p>
+                <p>bad {props.bad}</p>
+                <p>all {props.all}</p>
+                <p>average {props.avg}</p>
+                <p>positive {props.positive} %</p>
+            </>
+        )
+    }
     return (
         <>
-            <p>good { props.good }</p>
-            <p>neutral { props.neutral }</p>
-            <p>bad {props.bad}</p>
-            <p>all {props.all}</p>
-            <p>average {props.avg}</p>
-            <p>positive {props.positive} %</p>
+            <h2>Statistics</h2>
+            <p>No feedback given</p>
         </>
-    ) 
+    )
 }
 
 export default Statistics
