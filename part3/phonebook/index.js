ab102d35febf928d95b7f3eefbb0f82e79489c0d
Eggyf <abdelfregel1711@gmail.com>
2025-03-02 14:45:35 -0500

    part3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -17,7 +17,11 @@ const mongoose = require("mongoose");
 // mongoose.connect(url);
 
 const personSchema = new mongoose.Schema({
-  name: String,
+  name: {
+    type: String,
+    minLength: 5,
+    required: true,
+  },
   number: String,
 });
 
@@ -26,6 +30,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
   next(error);
 };
@@ -57,28 +63,28 @@ morgan(function (tokens, req, res) {
   ].join(" ");
 });
 
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+// let persons = [
+//   {
+//     id: "1",
+//     name: "Arto Hellas",
+//     number: "040-123456",
+//   },
+//   {
+//     id: "2",
+//     name: "Ada Lovelace",
+//     number: "39-44-5323523",
+//   },
+//   {
+//     id: "3",
+//     name: "Dan Abramov",
+//     number: "12-43-234345",
+//   },
+//   {
+//     id: "4",
+//     name: "Mary Poppendieck",
+//     number: "39-23-6423122",
+//   },
+// ];
 app.use(express.json());
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
@@ -91,31 +97,34 @@ app.use(express.static("dist"));
 //   return String(maxId + 1);
 // };
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
 
-  if (!body.name || !body.number) {
-    return response.status(400).json({
-      error: "name or number missing",
-    });
-  }
+  // if (!body.name || !body.number) {
+  //   return response.status(400).json({
+  //     error: "name or number missing",
+  //   });
+  // }
 
   const person = new Person({
     name: body.name,
     number: body.number,
   });
 
-  if (persons.find((person) => person.name === body.name) != undefined) {
-    return response.status(400).json({
-      error: "name must be unique",
-    });
-  }
+  // if (persons.find((person) => person.name === body.name) != undefined) {
+  //   return response.status(400).json({
+  //     error: "name must be unique",
+  //   });
+  // }
 
-  person.save().then((savedPerson) => {
-    response.json(savedPerson);
-  });
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
+    })
+    .catch((error) => next(error));
 
-  response.status(201).json(person);
+  // response.status(201).json(person);
 });
 
 app.get("/", (request, response) => {
@@ -124,6 +133,7 @@ app.get("/", (request, response) => {
 
 app.get("/api/persons", (request, response) => {
   console.log("phonebook:\n");
+
   Person.find({}).then((result) => {
     result.forEach((person) => {
       console.log(person);
@@ -165,14 +175,13 @@ app.delete("/api/persons/:id", (request, response) => {
 });
 
 app.put("/api/persons/:id", (request, response, next) => {
-  const body = request.body;
+  const { name, number } = request.body;
 
-  const person = {
-    name: body.name,
-    number: body.number,
-  };
-
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: "query" }
+  )
     .then((updatedPerson) => {
       response.json(updatedPerson);
     })
